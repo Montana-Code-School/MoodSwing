27cf2a67f8d5a01f5067bcb0bc132ce2a0a347c0
Katie <katie.pederson@gmail.com>
2018-02-17 14:16:25 -0700

    cleaned house

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import VRScene from './components/aframe/AFrame';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <VRScene/>
+      </div>
+    );
+  }
+}
+
+export default App;
