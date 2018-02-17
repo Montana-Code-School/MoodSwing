e73f699d00dd79bd18936e4a9751d5ecfed382a3
Katie <katie.pederson@gmail.com>
2018-02-17 11:01:31 -0700

    react app pop-up works

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import 'aframe';
+// import 'aframe';
 
 class App extends Component {
   render() {
@@ -14,17 +14,6 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-  <body>
-    hello world!!!! 
-    <a-scene>
-      <a-text value="Hello World" position="1 0.5 -3"></a-text>
-      <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
-      <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
-      <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
-      <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
-      <a-sky color="#ECECEC"></a-sky>
-    </a-scene>
-  </body>
       </div>
     );
   }
