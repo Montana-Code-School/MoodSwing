39e9fed828a556914530252dae8db11ce5419a68
Katie <katie.pederson@gmail.com>
2018-02-17 11:48:24 -0700

    A Frame on the page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import 'aframe';
+import VRScene from './components/aframe/AFrame';
 
 class App extends Component {
   render() {
@@ -14,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <VRScene/>
       </div>
     );
   }
