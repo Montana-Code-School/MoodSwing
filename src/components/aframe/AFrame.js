e864e854da53dc7a3b6761388d91c0fd2562bc0b
Gregory Lesky <sleepydistrict01@gmail.com>
2018-02-20 13:15:14 -0700

    Sky, plane, position slide bar

--- src/components/aframe/AFrame.js
+++ src/components/aframe/AFrame.js
@@ -2,16 +2,32 @@ import 'aframe';
 import 'aframe-particle-system-component';
 import {Entity, Scene} from 'aframe-react';
 import React from 'react';
+import winterSkyVR from './winterSkyVR.jpg';
 
 class VRScene extends React.Component {
   render () {
+    var myDate = new Date();
+    var hrs = myDate.getHours();
+
+    var greet;
+
+    if (hrs < 12)
+        greet = 'Good Morning';
+    else if (hrs >= 12 && hrs <= 17)
+        greet = 'Good Afternoon';
+    else if (hrs >= 17 && hrs <= 24)
+        greet = 'Good Evening';
+
     return (
       <Scene>
+        <Entity particle-system={{preset: 'snow', particleCount: 5000}}/>
         <Entity primitive='a-box' color="red" position="0 0 -5"/>
         <Entity primitive='a-sphere' color="green" position="-2 0 -3"/>
         <Entity primitive='a-cylinder' color="blue" position="2 0 -3"/>
-        <Entity primitive='a-sky' color="skyblue"/>
-        <Entity primitive='a-text' value="MOODSWING" color="black" position="1 3 -3"/>
+        <Entity primitive='a-sky' src={winterSkyVR}/>
+        <Entity primitive='a-text'scale="0.6 1.2 1" value="MOODSWING" color="black" position="-1 2.85 -3" width="3"/>
+        <Entity primitive='a-text' value={greet} color="black" position="-1 3 -3"/>
+        <Entity primitive='a-plane' rotation="-90 0 0" color="#CCC" height="10" width="10"/> 
       </Scene>
     );
   }
