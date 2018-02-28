d9cd009a4b27d7f2363d5bd817e73e27bbc7d8b1
Emily Yost <esyost@gmail.com>
2018-02-27 22:32:14 -0700

    text area

--- src/components/toneanalyzer/ToneAnalyzer.js
+++ src/components/toneanalyzer/ToneAnalyzer.js
@@ -1,20 +1,46 @@
 /* global chrome */
 
 import React from 'react';
+import Textarea from 'react-textarea-count';
 // import { Button } from 'semantic-ui-react';
 import "../../../node_modules/semantic-ui-forest-themes/semantic.darkly.css";
 import axios from 'axios';
 
 class ToneAnalyzer extends React.Component {
 
-componentDidMount() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      enteredMoodText: "What's on your mind?",
+      textMood: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log('Text to be analyzed: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  componentDidMount() {
   axios.get(
     "https://watson-api-explorer.mybluemix.net/tone-analyzer/api/v3/tone?text=" +
     "This%20is%20a%20test%20of%20this%20API%20but%20it%20needs%20some%20feelings%20and%20I%20am%20trying%20again!" +
     "&version=2018-02-27&sentences=true&tones=emotion"
+
+    // "https://watson-api-explorer.mybluemix.net/tone-analyzer/api/v3/tone?text=" +
+    // enteredMoodText +
+    // "&version=2018-02-27&sentences=true&tones=emotion"
   )
     .then((response) => {
       console.log(response)
+
     })
     .catch((error) => {
       console.log("Nope");
@@ -87,25 +113,21 @@ componentDidMount() {
   // //   console.log("it's working");
   // // };
   
-  render () {
-    
+  render() {
     return (
-      <div>
+      <form onSubmit={this.handleSubmit}>
         <div class="ui form segment">
-            <div class="field">
-              <label>What's on your mind?</label><textarea rows="2"></textarea>
-            </div>
-        <div className="ui animated button">
-        <div className="visible content" onClick={this.handleClickEvent}>
-          Analyze My Tone
-        </div>
-        <div className="hidden content">
-          Go!
-        </div>
+          <div class="field">
+        <label>
+          What's on your mind?
+          <Textarea />
+        </label>
+        <input className="ui submit button" type="submit" value="Analyze My Mood" onClick={this.handleClickEvent}>
+        </input>
           </div>
         </div>
-      </div>
-    )
+      </form>
+    );
   }
 }
 
