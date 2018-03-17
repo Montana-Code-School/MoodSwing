d0708eedba78ac79f51e64cf879d4ea7b463f7cd
Gregory Lesky <sleepydistrict01@gmail.com>
2018-03-17 13:53:42 -0600

    total VR usability

--- src/components/aframe/AFrame.js
+++ src/components/aframe/AFrame.js
@@ -38,30 +38,39 @@ class VRScene extends React.Component {
           {/* <audio id="click-sound" src="audio/click.ogg"></audio> */}
 
           {/* Images. */}
-          <img id="neutral" crossOrigin="anonymous" alt="Black cat, upright and filing its nails." src={groomSrc}/>
+          <img id="neutral" alt="Black cat, upright and filing its nails." src={groomSrc}/>
           <img id="puke" alt="360 degree world of green cubes layed out in rows." src={pukeSrc}/>
-          <img id="angrykitty" crossOrigin="anonymous" alt="Large, fluffy white cat with a birthday hat and cake looking very unhappy." src={angrykittySrc}/>
+          <img id="angrykitty" alt="Large, fluffy white cat with a birthday hat and cake looking very unhappy." src={angrykittySrc}/>
           <img id="climb" alt="climb" src={climbSrc}/>
-          <img id="kittykitty" crossOrigin="anonymous" alt="Small white kitten on its back with its paws stretched over its head." src={kitykittySrc}/>
-          <img id="balloon" crossOrigin="anonymous" alt="Small white kitten on its back with its paws stretched over its head." src={balloonSrc}/>
+          <img id="kittykitty" alt="Small white kitten on its back with its paws stretched over its head." src={kitykittySrc}/>
+          <img id="balloon" alt="Small white kitten on its back with its paws stretched over its head." src={balloonSrc}/>
 
 
         </a-assets>
-
-        {/* <Entity primitive='a-sky' src={winterSkyVR}/> */}
-
-        {/* <Entity particle-system={{preset: 'snow', particleCount: 5000}}/> */}
         
         {/* 360-degree image. */}
 
-        <Entity id="image-360" primitive='a-sky' radius="10" src={winterSkyVR}/>
+        <Entity id="image-360" primitive='a-sky' radius="10" src={balloonSrc}/>
 
         <Entity primitive='a-text'font= 'mozillavr' scale="0.6 1.2 1" value="If you need to talk to someone about how you're feeling, please call the Crisis Hotline 1-800-273-8255" color="black" position="-.75 2 -13" width="3" size="6"/>
         <Entity primitive='a-text'font= 'mozillavr' scale="0.6 1.2 1" value="HOLY CATZ!" color="black" position="-.75 2 -3" width="3" size="6"/>
         <Entity primitive='a-text' font= 'mozillavr' value={greet} color="black" position="-.85 1.85 -3"/>
 
         {/* Link we will build. */}
-        <Entity id="links" layout="layout: line; margin: 1.5" position="1.5 -1 -4">
+        {/* Camera + Cursor.  */}
+        <a-camera>
+        <a-cursor id="cursor" 
+          animation-click="property: scale; startEvents: click; from: 0.1 0.1 0.1; to: 1 1 1; dur: 150" 
+          animation-fusing="property: fusing; startEvents: fusing; from: 1 1 1; to: 0.1 0.1 0.1; dur: 1500" 
+          event-set__1="_event: mouseenter; color: springgreen" 
+          event-set__2="_event: mouseleave; color: yellow" 
+          fuse="true" 
+          raycaster="objects: .link" 
+          color="yellow" 
+          fusing="0.09999999999999998 0.09999999999999998 0.09999999999999998 ">
+        </a-cursor>
+        </a-camera>
+          <Entity id="links" layout="layout: line; margin: 1.5" position="1.5 -1 -4">
           <Entity class="link" 
             position="-3"
             geometry="primitive: plane; height: 1; width: 1" 
@@ -82,22 +91,10 @@ class VRScene extends React.Component {
             event-set="_event: mousedown; on: click; _target: #image-360; src: #climb" >
             {/* sound="on: click; src: #click-sound"> */}
           </Entity>
+        
         </Entity>
 
-        {/* Camera + Cursor.  */}
-        <a-entity camera="" look-controls="">
-        <a-cursor id="cursor" 
-          animation-click="property: scale; startEvents: click; from: 0.1 0.1 0.1; to: 1 1 1; dur: 150" 
-          animation-fusing="property: fusing; startEvents: fusing; from: 1 1 1; to: 0.1 0.1 0.1; dur: 1500" 
-          event-set__1="_event: mouseenter; color: springgreen" 
-          event-set__2="_event: mouseleave; color: yellow" 
-          fuse="true" 
-          raycaster="objects: .link" 
-          color="yellow" 
-          fusing="0.09999999999999998 0.09999999999999998 0.09999999999999998 ">
-        </a-cursor>
-        </a-entity>
-        <a-entity light="" data-aframe-default-light="" aframe-injected="" position="" rotation="" scale="" visible=""></a-entity>
+       
       </Scene>
     );
   }
